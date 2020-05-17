610a7b3e29ef75d0dd29b06f388d601b64148a93
dbr <jhonatansantiago242012@gmail.com>
2020-05-17 16:03:24 -0300

    enviado

--- bot.js
+++ bot.js
@@ -17,19 +17,6 @@ client.on("ready", () => {
   // docs refer to as the "ClientUser".
   client.user.setActivity('ALFA RUST');
 });
-//heroku
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-
-express()
-.use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, 'public'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/index'))
-.listen(port, () => console.log('Listening on ${ PORT }'))
-
-
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
